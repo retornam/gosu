9b1b0e8449182c12903c1ae06102d9747700b8df
retornam <retornam@users.noreply.github.com>
2026-06-03 15:49:39 -0700

    modifications
    Signed-off-by: Raymond Etornam <retornam@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 set -x
 
-docker build --pull -t gosu .
+docker build --pull -t retornam/gosu .
 
 rm -f gosu* SHA256SUMS*
-docker run --rm gosu sh -c 'cd /go/bin && tar -c gosu*' | tar -xv
+docker run --rm retornam/gosu sh -c 'cd /go/bin && tar -c gosu*' | tar -xv
 sha256sum gosu* | tee SHA256SUMS
 file gosu*
 ls -lFh gosu* SHA256SUMS*
